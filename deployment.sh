cd8deae14df2fba135c04f956f2a9f67d25c561d
Jordi Farré <jordi.farre@mango.com>
2017-08-23 21:41:21 +0200

    added circleci configuration and lambda deployment script

---
+++ deployment.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+configure() {
+	aws configure set default.region us-east-1
+	aws configure set output json
+}
+
+createLambdaFunction() {
+	if aws lambda get-function --function-name ${FUNCTION_NAME} --query 'Configuration.FunctionName'
+	then
+		aws lambda update-function-code --function-name $FUNCTION_NAME --s3-bucket ${S3_BUCKET} --s3-key ${FILE_NAME}
+	else
+		aws lambda create-function --region us-east-1 --function-name $FUNCTION_NAME --code S3Bucket=${S3_BUCKET},S3Key=${FILE_NAME} --role arn:aws:iam::175801550592:role/lambda_basic_execution --handler tee.finder.qwertee.Hello::handleRequest --runtime java8
+	fi
+}
+
+FILE_NAME="target/qwertee-1.0-SNAPSHOT.jar"
+FUNCTION_NAME="qwertee-tee-finder"
+S3_BUCKET="daily-tee-finder-deployment"
+configure
+createPackage
+uploadArtifactS3
+rm ${FILE_NAME}
+createLambdaFunction
