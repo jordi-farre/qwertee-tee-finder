fc51c90f37341a1cc79bcb5f5c96d81f3ad211d8
Jordi Farré <jordi.farre@mango.com>
2017-08-24 18:43:49 +0200

    added qwertee expected response and test skeleton

---
+++ src/test/java/tee/finder/qwertee/GetAndStoreTeesFeature.java
@@ -0,0 +1,12 @@
+package tee.finder.qwertee;
+
+import org.junit.Test;
+
+public class GetAndStoreTeesFeature {
+    
+    @Test
+    public void get_site_information_from_rss_and_store() {
+
+    }
+    
+}
