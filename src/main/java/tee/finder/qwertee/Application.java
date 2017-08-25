ddb08b5fcd5c36ae87800caae5793b638d7f3812
Jordi Farré <jordi.farre@mango.com>
2017-08-25 17:09:36 +0200

    lambda main application implemented

--- src/main/java/tee/finder/qwertee/Application.java
+++ src/main/java/tee/finder/qwertee/Application.java
@@ -10,23 +10,26 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.S3Object;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Application implements RequestHandler<ScheduledEventDto, String> {
 
     public String handleRequest(ScheduledEventDto input, Context context) {
-        return "Application " + input;
+        try {
+            SiteClient siteClient = new SiteClient(new URL("https://www.qwertee.com/rss"));
+            AmazonS3 s3Client = AmazonS3ClientBuilder.standard().build();
+            SiteRepository siteRepository = new SiteAmazonS3Repository(s3Client, "site-tees-production");
+            SiteService siteService = new SiteService(siteClient, siteRepository);
+            siteService.getAndStoreInformation();
+        } catch (MalformedURLException e) {
+            return "Error: " + e.getMessage();
+        }
+        return "OK";
     }
 
-    public static void main(String[] args) {
-        BasicAWSCredentials awsCreds = new BasicAWSCredentials("AKIAIXIAXLVEIY3MZBZQ", "p1ETpXepOtexQ/9NzzUV9Aht58v6s11Ibg5ODVHm");
-        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(awsCreds)).withRegion(Regions.US_EAST_1)
-                .build();
-        s3Client.putObject("site-tees", "development/test", "newContent");
-        S3Object test = s3Client.getObject("site-tees", "test");
-        System.out.println(new Scanner(test.getObjectContent(), "UTF-8").useDelimiter("\\A").next());
-    }
+
 
 }
