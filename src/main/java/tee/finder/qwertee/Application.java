2dd0ac2537bec053e690af55ab2ddebae3471d20
Jordi Farré <jordi.farre@mango.com>
2017-08-28 14:55:43 +0200

    added traces

--- src/main/java/tee/finder/qwertee/Application.java
+++ src/main/java/tee/finder/qwertee/Application.java
@@ -9,6 +9,8 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.S3Object;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -17,8 +19,11 @@ import java.util.Scanner;
 
 public class Application implements RequestHandler<ScheduledEventDto, String> {
 
+    private Logger LOG = LoggerFactory.getLogger(Application.class);
+
     public String handleRequest(ScheduledEventDto input, Context context) {
         try {
+            LOG.info("Application startup");
             SiteClient siteClient = new SiteClient(new URL("https://www.qwertee.com/rss"));
             AmazonS3 s3Client = AmazonS3ClientBuilder.standard().build();
             SiteRepository siteRepository = new SiteAmazonS3Repository(s3Client, "site-tees-production");
