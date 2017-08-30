727394a87667f2b0a3929f8218125132830d6994
Jordi Farré <jordi.farre@mango.com>
2017-08-31 00:48:20 +0200

    fix for set environment variables on update

--- deployment.sh
+++ deployment.sh
@@ -21,7 +21,8 @@ uploadArtifactS3() {
 createLambdaFunction() {
 	if aws lambda get-function --function-name ${FUNCTION_NAME} --query 'Configuration.FunctionName'
 	then
-		aws lambda update-function-code --function-name $FUNCTION_NAME --s3-bucket ${S3_BUCKET} --s3-key ${RELEASE_NAME} --environment Variables={QWERTEE_URL=https://www.qwertee.com/rss,S3_BUCKET=site-tees-production}
+	    aws lambda update-function-configuration --function-name $FUNCTION_NAME --environment Variables={QWERTEE_URL=https://www.qwertee.com/rss,S3_BUCKET=site-tees-production}
+		aws lambda update-function-code --function-name $FUNCTION_NAME --s3-bucket ${S3_BUCKET} --s3-key ${RELEASE_NAME}
 	else
 		aws lambda create-function --region us-east-1 --function-name $FUNCTION_NAME --code S3Bucket=${S3_BUCKET},S3Key=${RELEASE_NAME} --role ${ROLE_ARN} --handler tee.finder.qwertee.Application::handleRequest --runtime java8 --timeout 30 --memory-size 256 --environment Variables={QWERTEE_URL=https://www.qwertee.com/rss,S3_BUCKET}
 	fi
