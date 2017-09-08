601f03bcf1228f7f461a3a9ee4e55e7acfcca839
Jordi Farré <jordi.farre@mango.com>
2017-09-08 13:50:50 +0200

    fixed application error to test application is deployed correctly

--- src/main/java/tee/finder/qwertee/Application.java
+++ src/main/java/tee/finder/qwertee/Application.java
@@ -15,18 +15,17 @@ public class Application implements RequestHandler<ScheduledEventDto, String> {
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
     public String handleRequest(ScheduledEventDto input, Context context) {
-        throw new RuntimeException();
-        //try {
-          //  LOGGER.info("Application startup");
-            //SiteClient siteClient = new SiteClient(new URL(System.getenv("QWERTEE_URL")));
-            //AmazonS3 s3Client = AmazonS3ClientBuilder.standard().build();
-            //SiteRepository siteRepository = new SiteAmazonS3Repository(s3Client, System.getenv("S3_BUCKET"));
-            //SiteService siteService = new SiteService(siteClient, siteRepository);
-            //siteService.getAndStoreInformation();
-        //} catch (MalformedURLException e) {
-          //  LOGGER.error("Error", e);
-        //}
-        //return "OK";
+        try {
+            LOGGER.info("Application startup");
+            SiteClient siteClient = new SiteClient(new URL(System.getenv("QWERTEE_URL")));
+            AmazonS3 s3Client = AmazonS3ClientBuilder.standard().build();
+            SiteRepository siteRepository = new SiteAmazonS3Repository(s3Client, System.getenv("S3_BUCKET"));
+            SiteService siteService = new SiteService(siteClient, siteRepository);
+            siteService.getAndStoreInformation();
+        } catch (MalformedURLException e) {
+            LOGGER.error("Error", e);
+        }
+        return "OK";
     }
 
 }
