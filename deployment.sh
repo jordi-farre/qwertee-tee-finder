14dce7f5ad4d81c619cbadc747fc99107241f15a
Jordi Farré <jordi.farre@mango.com>
2017-09-08 12:44:56 +0200

    add logs to deployment script

--- deployment.sh
+++ deployment.sh
@@ -11,20 +11,25 @@ createRole() {
     aws iam put-role-policy --role-name ${ROLE_NAME} --policy-name s3 --policy-document file://s3PolicyFile.json
     aws iam put-role-policy --role-name ${ROLE_NAME} --policy-name logs --policy-document file://logPolicyFile.json
     ROLE_ARN=$(aws iam get-role --role-name ${ROLE_NAME} | ./jq.sh -r '.Role.Arn')
+    echo "Role $ROLE_NAME created with arn $ROLE_ARN"
 }
 
 uploadArtifactS3() {
     mv target/${FILE_NAME} ${RELEASE_NAME}
 	aws s3 cp ${RELEASE_NAME} s3://${S3_BUCKET}/
+    echo "File $RELEASE_NAME copied to $S3_BUCKET bucket"
 }
 
 createFunction() {
     FUNCTION_VERSION=$(aws lambda create-function --region us-east-1 --function-name $FUNCTION_NAME --code S3Bucket=${S3_BUCKET},S3Key=${RELEASE_NAME} --role ${ROLE_ARN} --handler tee.finder.qwertee.Application::handleRequest --runtime java8 --timeout 30 --memory-size 256 --environment Variables={QWERTEE_URL=https://www.qwertee.com/rss,S3_BUCKET=site-tees-production} --publish | ./jq.sh -r '.Version')
+    echo "Function $FUNCTION_NAME created with version $FUNCTION_VERSION"
     FUNCTION_ERROR=$(aws lambda invoke --function-name $FUNCTION_NAME /dev/null | ./jq.sh -r ".FunctionError")
+    echo "Function $FUNCTION_NAME executed with error $FUNCTION_ERROR"
     if [ -z "$FUNCTION_ERROR" ]; then
         exit 1
     else
         aws lambda create-alias --function-name $FUNCTION_NAME --name PRODUCTION --function-version $FUNCTION_VERSION
+        echo "Alias PRODUCTION created for $FUNCTION_NAME with version $FUNCTION_VERSION"
     fi
 }
 
@@ -32,11 +37,14 @@ updateFunction() {
     aws lambda update-function-configuration --function-name $FUNCTION_NAME --environment Variables={QWERTEE_URL=https://www.qwertee.com/rss,S3_BUCKET=site-tees-production}
     aws lambda update-function-code --function-name $FUNCTION_NAME --s3-bucket ${S3_BUCKET} --s3-key ${RELEASE_NAME}
     FUNCTION_VERSION=$(aws lambda publish-version --function-name $FUNCTION_NAME | ./jq.sh -r '.Version')
+    echo "Function $FUNCTION_NAME created with version $FUNCTION_VERSION"
     FUNCTION_ERROR=$(aws lambda invoke --function-name $FUNCTION_NAME /dev/null | ./jq.sh -r ".FunctionError")
+    echo "Function $FUNCTION_NAME executed with error $FUNCTION_ERROR"
     if [ -z "$FUNCTION_ERROR" ]; then
         exit 1
     else
         aws lambda update-alias --function-name $FUNCTION_NAME --name PRODUCTION --function-version $FUNCTION_VERSION
+        echo "Alias PRODUCTION updated for $FUNCTION_NAME with version $FUNCTION_VERSION"
     fi
 }
 
